e9deac7d7dcb0932f890b06f5e6eb3d4f4a4855a
jeongminseok <minseok.jeong.james@gmail.com>
2022-11-29 19:35:23 +0900

    14주차 실습 - foreground service

--- oop_class/week_14/app/src/main/java/com/example/myactivityapplication/MainActivity.kt
+++ oop_class/week_14/app/src/main/java/com/example/myactivityapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myactivityapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.myactivityapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +20,13 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+
+        binding.btnService.setOnClickListener {
+            val intent = Intent(this, ProgressService::class.java)
+
+//            startService(intent)
+            ContextCompat.startForegroundService(this,intent)
+        }
+
     }
 }
